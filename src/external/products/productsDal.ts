6264af41d996639c47fee8031d272f3bfc398020
unknown <arish186777@gmail.com>
2023-11-20 15:36:09 +0200

    products end point external fix bug chenge to axios

--- src/external/products/productsDal.ts
+++ src/external/products/productsDal.ts
@@ -1,18 +1,35 @@
-import { Product } from "../../internal/products/productModel";
+import { Product } from "../../types";
+import axios from "axios";
 
 export const getAllProductsDal = async () => {
-  const products = await Product.find({});
-  console.log(products);
-  return products;
+  try {
+    const productsResult = await axios.get("/api/products");
+    const products: Product[] = productsResult.data;
+    if (productsResult.status === 200) {
+      return products;
+    } else {
+      throw { status: 404, message: `Product not found` };
+    }
+  } catch (error) {
+    console.error(error);
+    throw { status: 500, message: `Internal Server Error` };
+  }
 };
 
-export const productDal = async (productId: string) => {
+export const getProductDal = async (productId: string) => {
+  const hasRequiredKeys = (obj: Record<string, any>, keys: string[]): obj is Product => {
+    return keys.every((key) => key in obj);
+  };
   try {
-    const productResult = await Product.findOne({ _id: productId });
-    if (productResult) {
-      return productResult;
-    } else throw Error(`Product not  find (dal erorrs)`);
-  } catch (err) {
-    return err;
+    const productResult = await axios.get(`/api/products/${productId}`);
+    const productData: Product = productResult.data;
+    if (productResult.status === 200 && hasRequiredKeys(productData, ["name", "salePrice", "quantity", "description", "category", "discountPercentage", "image"])) {
+      return productData;
+    } else {
+      throw { status: 404, message: `Product not found` };
+    }
+  } catch (error) {
+    console.error(error);
+    throw { status: 500, message: `Internal Server Error` };
   }
 };
--- src/external/products/productsRouter.ts
+++ src/external/products/productsRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllProducts ,product} from "./productsController";
+import { getAllProducts, product } from "./productsController";
 
 const router = express.Router();
 

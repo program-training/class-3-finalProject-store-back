8ff7b008f5a7b9fda8f65d9d97d7cc52b0801f91
unknown <arish186777@gmail.com>
2023-11-20 14:09:29 +0200

    first version to end point external

--- src/external/products/productsService.ts
+++ src/external/products/productsService.ts
@@ -1,1 +1,4 @@
-export const test = async () => {};
+import { getAllProductsDal , productDal } from "./productsDal";
+
+export const getAllProductsService = async () => await getAllProductsDal();
+export const productGetService = async (productId: string) => await productDal(productId);

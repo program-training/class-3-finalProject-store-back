4e8203fb62f7a49a9783074c42959988fceb69ee
moishy friedman <moshehlf@gmail.com>
2023-11-21 10:03:00 +0200

    external api

--- src/external/orders/orderController.ts
+++ src/external/orders/orderController.ts
@@ -1,3 +1,8 @@
 import { Request, Response } from "express";
+import { getOrderByUserService } from "./orderServices";
 
-export const signUpUser = async (req: Request, res: Response) => {};
+export const getOrderByUser = async (req: Request, res: Response) => {
+  try {
+    const response = await getOrderByUserService();
+  } catch (error) {}
+};

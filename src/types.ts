07e4e1a049f7d43a87bfd993693a282dfff998fa
moishy friedman <moshehlf@gmail.com>
2023-11-21 09:42:24 +0200

    external api

--- src/types.ts
+++ src/types.ts
@@ -14,7 +14,18 @@ interface User {
   password: string;
 }
 
-interface Product {}
+interface Product {
+  name: string;
+  salePrice: number;
+  quantity: number;
+  description: string;
+  category: string;
+  discountPercentage: number;
+  image: {
+    url: string;
+    alt: string;
+  };
+}
 interface Category {}
 
 interface Order {

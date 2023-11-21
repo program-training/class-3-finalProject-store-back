f193f8d82d5cd833c4485efb5157907a3947b92d
Shimon-Aizenbach <s0533137261@gmail.com>
2023-11-21 16:10:59 +0200

    now its fix

--- src/server.ts
+++ src/server.ts
@@ -3,25 +3,19 @@ import morgan from "morgan";
 import cors from "cors";
 import { config } from "./config";
 import { connectionToDB } from "./connectionToDB";
-<<<<<<< HEAD
 import {authenticateToken} from "./helpers/jwt"
 import usersRouter from "./internal/users/usersRouter"
-=======
 import routerOrder from "./external/orders/orderRouter";
->>>>>>> develop
 
 const app = express();
 
 app.use(cors());
 app.use(morgan(`tiny`));
 app.use(express.json());
-<<<<<<< HEAD
 app.use(authenticateToken);
 app.use(`/users`, usersRouter)
 
-=======
 app.use("/orders", routerOrder);
->>>>>>> develop
 
 app.listen(config.server.port, () => {
   console.log(`Server is running on port ${config.server.port}`);

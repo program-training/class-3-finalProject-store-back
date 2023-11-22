d79bf30afd9f88b7052982ad317bb49be57122b3
Boaz Pash <boazpash123@gmail.com>
2023-11-22 14:53:18 +0200

    adding logs

--- src/internal/users/usersDal.ts
+++ src/internal/users/usersDal.ts
@@ -11,12 +11,13 @@ export const userRegisterDal = async (user: User) => {
     if (uniquenessCheck) throw Error(`This user is already registered!`);
     const newUser = new UserDB(user);
     const userRegister = await newUser.save();
+    console.log(userRegister);
     if (userRegister) {
       const token = createToken(user);
+      console.log(token);
       return token;
     }
   } catch (err) {
-    // console.error(chalk.redBright(err));
     console.error(err);
     return Promise.reject(err);
   }

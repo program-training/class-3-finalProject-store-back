4625cea3f61a2d0d7a84a135f943de3c4753612a
unknown <arish186777@gmail.com>
2023-12-10 11:50:44 +0200

    convert usersdal to postgres

--- src/users/usersDal.ts
+++ src/users/usersDal.ts
@@ -1,39 +1,63 @@
 import { User } from "../helpers/types";
-import { UserDB } from "../DB/models/userModel";
+import { Pool, Client } from "pg";
 import { createToken } from "../helpers/jwt";
 import { comparePassword } from "../helpers/bcrypt";
 
+const pool = new Pool();
+
 export const userRegisterDal = async (user: User) => {
-  const { email } = user;
+  const { email, password } = user;
+  const client = await pool.connect();
   try {
-    const uniquenessCheck = await UserDB.findOne({ email: email });
-    if (uniquenessCheck) throw Error(`This user is already registered!`);
-    const newUser = new UserDB(user);
-    const userRegister = await newUser.save();
-    console.log(userRegister);
-    if (userRegister) {
-      const token = createToken(user);
+    const checkEmailQuery = await client.query("SELECT * FROM users WHERE email = $1", [email]);
+    if (checkEmailQuery.rows.length > 0) {
+      throw new Error(`This user is already registered!`);
+    }
+
+    const insertUserQuery = "INSERT INTO users(email, password) VALUES($1, $2) RETURNING *";
+    const newUserResult = await pool.query(insertUserQuery, [email, password]);
+
+    const newUser = newUserResult.rows[0];
+    console.log(newUser);
+
+    if (newUser) {
+      const token = createToken(newUser);
       return token;
     }
   } catch (err) {
     return Promise.reject(err);
+  } finally {
+    client.release();
   }
 };
 
 export const userLoginDal = async (user: User) => {
+  const { email, password } = user;
+  const client = await pool.connect();
+
   try {
-    const { email, password } = user;
-    const userFromDB = await UserDB.findOne({ email: email });
-    if (!userFromDB) throw Error(`User not found`);
-    const comparePasswordFromUser = comparePassword(
-      password,
-      userFromDB.password
-    );
-    if (!comparePasswordFromUser) throw Error(`Password is incorrect`);
-    const userFromDBObject: User = JSON.parse(JSON.stringify(userFromDB));
+    const getUserQueryResult = await client.query("SELECT * FROM users WHERE email = $1", [email]);
+    const userFromDB = getUserQueryResult.rows[0];
+
+    if (!userFromDB) {
+      throw new Error(`User not found`);
+    }
+
+    const comparePasswordFromUser = comparePassword(password, userFromDB.password);
+    if (!comparePasswordFromUser) {
+      throw new Error(`Password is incorrect`);
+    }
+
+    const userFromDBObject: User = {
+      email: userFromDB.email,
+      password: userFromDB.password,
+    };
+
     const token = createToken(userFromDBObject);
     return token;
   } catch (err) {
     return Promise.reject(err);
+  } finally {
+    client.release();
   }
 };

74d7e3abd5602ad550baf2aec24aafdce983806b
Shimon-Aizenbach <s0533137261@gmail.com>
2023-12-10 12:23:03 +0200

    add logic to time trigger endpoint with git pull -fix

--- src/triggers/triggersDal.ts
+++ src/triggers/triggersDal.ts
@@ -4,12 +4,7 @@ import { CartReportsModel } from "../monggoDB/models/cartReportModel";
 export const getTimeTriggerDal = async () => {
   try {
     const cartReports: CartReport[] = await CartReportsModel.find({});
-<<<<<<< HEAD
     return cartReports
-=======
-    console.log(cartReports);
-    return cartReports;
->>>>>>> develop
   } catch (error) {
     console.log(error);
     return Promise.reject(error);

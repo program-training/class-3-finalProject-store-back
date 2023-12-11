0fa35f5c8e4a53e5f02e6489b3d26138d5abcf70
unknown <arish186777@gmail.com>
2023-12-11 10:23:47 +0200

    add types to grapQL fix files

--- src/graphql/types.ts
+++ src/graphql/types.ts
@@ -1,50 +1,105 @@
-export const typeDefs = `
-type Hello{
-    text:String
-    number:Int
-}
-type Query{
-    hello:Hello
-    getProducts:[IProduct]
-    findById(_id:String!):IProduct
+const typeDefs = `#graphql
+type Image {
+  large: String!
+  medium: String!
+  small: String!
+  alt: String!
 }
 
-type IProduct{
-    name:String!
-    description:String
-    price:Int!
-    quantity:Int!
-    image:String
-    _id:String
-}
-type IUser{
-    _id:ID!
-    email: String!
-    password: String!
-}
-input UserInput{
-    email: String!
-    password: String!
-}
-input ProductData{
-    name:String
-    description:String
-    price:Int
-    quantity:Int
-    image:String
-}
-type Mutation{
-    signUp(user:UserInput!):IUser!
-    updateProduct(_id:String! productData:ProductData!):IProduct
-    deleteProduct(_id:String!):IProduct
+type Product {
+  _id: String!
+  name: String!
+  salePrice: Float!
+  quantity: Int!
+  description: String!
+  category: String!
+  discountPercentage: Float!
+  image: Image!
 }
 
-type Subscription{
-    productCreated:IProduct
+type CartItem {
+  userId: String!
+  product: Product!
 }
-schema {
-    query:Query
-    mutation:Mutation
-    subscription:Subscription
-} 
+
+type Checkout {
+  orderId: String!
+  cartItems: [CartItem!]!
+}
+
+type User {
+  _id: String
+  email: String!
+  password: String!
+}
+
+type Category {
+  _id: String!
+  name: String!
+  img: String!
+  __v: Int!
+}
+
+type OrderProduct {
+  _id: String!
+  name: String!
+  description: String!
+  price: Float!
+  quantity: Int!
+}
+
+type ShippingDetails {
+  address: String!
+  userId: String!
+  contactNumber: String!
+  orderType: String!
+}
+
+type Order {
+  id: String!
+  cartItems: [OrderProduct!]!
+  orderTime: String!
+  status: String!
+  price: Float!
+  shippingDetails: ShippingDetails!
+}
+
+input UserInput {
+  email: String
+  password: String
+}
+
+input DeleteCartInput {
+  productId: String
+  userId: String
+  }
+
+enum OrderStatus {
+  PENDING
+  DELIVERED
+  REFUNDED
+}
+
+enum OrderType {
+  PICKUP
+  EXPRESS
+  SHIPPING
+}
+
+type Query {
+  getAllProducts(categoryName: String): [Product]!
+  getProduct(productId: ID!): Product
+  getCategories: [Category]!
+  similarProducts(categoryName: String, quantity: Int): [Product]!
+  getOrderByUser(userId: String!): Order
+  getCartByUser(userId: String!): [Cart]
+}
+
+type Mutation {
+  register(userInput: UserInput!): String
+  login(userInput: UserInput!): String
+  postOrderCart(order: Order!): Order
+  addCartItem( cartItem: CartItem!): cartItem
+  deleteCartItem(deleteCartInput: DeleteCartInput ): cartItem
+}
 `;

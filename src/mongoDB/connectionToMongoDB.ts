4afe62c72daf27c291f4fd9ba800f699d32eb025
moishy friedman <moshehlf@gmail.com>
2023-12-11 09:43:06 +0200

    fix users endpoint to work with postgres

--- src/mongoDB/connectionToMongoDB.ts
+++ src/mongoDB/connectionToMongoDB.ts
@@ -4,7 +4,7 @@ export const connectionToMongoDB = () => {
   mongoose
     .connect(process.env.MONGO_CONNECTION_URI || "", { retryWrites: true, w: "majority" })
     .then(() => {
-      console.log("Connected to mongoDB");
+      console.log("connection to mongoDB");
     })
     .catch((error) => {
       console.log(error);
